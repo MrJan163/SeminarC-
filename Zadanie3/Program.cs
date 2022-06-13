2c68a48538cbba690620c673cf5a6534a233cf76
Leoshkevich Jan <Mr.Leoshkevich.Jan@mail.ru>
2022-06-13 17:05:05 +0400

    Чет-нечет

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,16 @@
+﻿int q;
+Console.Write("Введите число : ");
+
+q = int.Parse(Console.ReadLine());
+
+    if (q % 2 == 0)
+    {
+        Console.WriteLine("Четное");
+        Console.Read();
+    }
+    else
+    {
+        Console.WriteLine("Нечетное");
+        Console.Read();
+    }
+
