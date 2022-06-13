69b4cd641ca334c7cbae0c4b04e12be0c7e43087
Leoshkevich Jan <Mr.Leoshkevich.Jan@mail.ru>
2022-06-13 14:13:20 +0400

    Больше,меньше

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите два числа: ");
+
+int a, b;
+ 
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+         {
+             Console.WriteLine("{0} наибольшее число", a);
+         }
+        else
+         {
+             Console.WriteLine("{0} наибольшее число ", b);
+         }
+         if (a < b)
+         {
+             Console.WriteLine("{0} наименьшее число", a);
+         }
+        else
+         {
+             Console.WriteLine("{0} наименьшее число ", b);
+         }
+ 
+        Console.ReadLine();
+ 
+    
